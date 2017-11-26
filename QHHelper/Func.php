7d2aa920ca438f950fe9d59356d5a8d4f37140e2
VuLe <vu.lehoangthanh16051991@gmail.com>
2017-11-26 10:00:23 +0700

    Update 1.2.7

---
+++ QHHelper/Func.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace QHHelper;
+
+class Func
+{
+    /**
+     * Convert the string with hyphens to StudlyCaps,
+     * e.g. post-authors => PostAuthors
+     *
+     * @param string $string The string to convert
+     *
+     * @return string
+     */
+    public function convertToStudlyCaps($string)
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $string)));
+    }
+
+    /**
+     * Convert the string with hyphens to camelCase,
+     * e.g. add-new => addNew
+     *
+     * @param string $string The string to convert
+     *
+     * @return string
+     */
+    protected function convertToCamelCase($string)
+    {
+        return lcfirst($this->convertToStudlyCaps($string));
+    }
+}
